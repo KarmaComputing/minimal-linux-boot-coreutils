3e7e5732c88e77279cad06bd925b7363fa2e392e
chrisjsimpson <chris.j.simpson@live.co.uk>
2024-12-22 22:06:49 +0000

    example record the boot up process

--- run-qemu.sh
+++ run-qemu.sh
@@ -2,4 +2,7 @@
 
 set -eux
 
-qemu-system-x86_64 -kernel ./build-dir/linux-kernel/linux-6.9/arch/x86/boot/bzImage -initrd rootfs.cpio.gz --append "console=ttyS0 init=/init" -nographic # -icount 10,align=on
+qemu-system-x86_64 -kernel ./build-dir/linux-kernel/linux-6.9/arch/x86/boot/bzImage -initrd rootfs.cpio.gz --append "console=ttyS0 init=/init" -nographic  # -icount 10,align=on
+
+# Example: Record the output terminal of the bootup:
+# timeout --preserve-status 20 script -c ./run-qemu.sh output.log
