42bd68179a0d6e32e60408d60ecbdbc3ec1c4e9d
chrisjsimpson <chris.j.simpson@live.co.uk>
2024-12-26 14:59:15 +0000

    added make-iso.sh

--- run-qemu.sh
+++ run-qemu.sh
@@ -2,7 +2,11 @@
 
 set -eux
 
-qemu-system-x86_64 -kernel ./build-dir/linux-kernel/linux-6.9/arch/x86/boot/bzImage -initrd rootfs.cpio.gz --append "console=ttyS0 init=/init" -nographic  # -icount 10,align=on
+qemu-system-x86_64 -enable-kvm -kernel ./build-dir/linux-kernel/linux-6.9/arch/x86/boot/bzImage -initrd rootfs.cpio.gz --append "console=ttyS0 init=/init ip=192.168.10.2:192.168.10.1:192.168.10.1:255.255.255.0::eth0:off" -nographic \
+  -netdev tap,id=net0,ifname=tap-ssh-node-0,script=no,downscript=no\
+  -device e1000,netdev=net0
+
+# -icount 10,align=on
 
 # Example: Record the output terminal of the bootup:
 # timeout --preserve-status 20 script -c ./run-qemu.sh output.log
